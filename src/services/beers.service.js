a99fe783663ec33a90411fbca8a5d0171079e8b3
Oleg Grunov <grunov.co@gmail.com>
2022-04-22 14:14:43 +0300

    dist

--- src/services/beers.service.js
+++
@@ -1,7 +0,0 @@
-import $api from "@/http";
-
-export default class BeersService {
-    static getRandomBeer() {
-        return $api.get('/beer/random_beer');
-    }
-}
--- src/services/users.service.js
+++
@@ -1,7 +0,0 @@
-import $api from "@/http";
-
-export default class UsersService {
-    static getRandomUser() {
-        return $api.get('/users/random_user');
-    }
-}
